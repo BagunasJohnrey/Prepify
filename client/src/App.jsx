14e03bd53f263476c204a396c6bfd357db22181e
BagunasJohnrey <johnreybagunas2@gmail.com>
2025-12-01 02:37:17 +0800

    fix layout path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import Layout from './components/layout/Layout';
+import Layout from './layout/Layout';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
